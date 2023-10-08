fb28743bf0856433026ce9661f3e8749d5285b63
elmiraaemi <108354246+elmiraaemi@users.noreply.github.com>
2023-10-08 19:50:03 +0330

    Time.py

--- time.py
+++ time.py
@@ -1,42 +1,42 @@
+import datetime
+import pytz
+
 class Time:
     #properties
     def __init__(self, h, m, s):
         self.hour = h
         self.minute = m
         self.second = s
+        self.fix()
 
     #methods
     def time_to_second(self):
         result_h = self.hour * 3600
         result_m = self.minute * 60
         result_s = self.second + result_h + result_m
-        return result_s
+        x = Time(0, 0, result_s)
+        return x
     
     def second_to_time(self):
         if 60 <= self.second <3600 :
-            result_m = int( self.second / 60)
+            result_m = self.second // 60
             c = result_m * 60
             result_s = self.second - c
-            # print(result_m , ":" , result_s)
-            x = Time(0, result_m, result_s)
-            return x
+            result_h = 0
         elif self.second >= 3600 :
-            result_h = int( self.second / 3600)
+            result_h = self.second // 3600
             e = result_h * 3600
             result_s = self.second - e
             if result_s < 60 :
-                # print(result_h , ":" , "00" , ":" , result_s )
-                x = Time(result_h, 00, result_s)
-                return x
-            if result_s > 60 :
-                result_m = int( result_s / 60)
+                result_m =0
+            else :
+                result_m = result_s // 60
                 k = result_m * 60
                 result_s = result_s - k
-                # print(result_h , ":" , result_m , ":" , result_s )
-                x = Time(result_h, result_m, result_s)
-                return x
         elif self.second < 60 :
-            print("Less than a minute")
+            result_h = 0
+            result_m = 0
+            result_s = self.second
         x = Time(result_h, result_m, result_s)
         return x
     
@@ -85,7 +85,37 @@ class Time:
                 result_h = self.hour - other.hou
         x = Time(result_h, result_m, result_s)
         return x
+
+    def GMT_to_tehran_time(self) :
+        result_h = self.hour + 3
+        result_m = self.minute + 30 
+        result_s = self.second 
+        x = Time(result_h, result_m, result_s)
+        return x
     
+    #     gmt = pytz.timezone('GMT')
+    #     tehran = pytz.timezone('Asia/Tehran')
+    #     gmt_time = datetime.datetime.now(tz=gmt)
+    #     tehran_time = gmt_time.astimezone(tehran)
+    #     print(tehran_time, ": زمان فعلی تهران ")
+
+    def fix(self):
+        if self.second >= 60 :
+            self.second -= 60
+            self.minute += 1 
+
+        if self.minute >= 60 :       
+            self.minute -= 60 
+            self.hour += 1
+
+        if self.second < 0 : 
+            self.second += 60 
+            self.minute -= 1
+
+        if self.minute < 0 :
+            self.minute += 60
+            self.hour -= 1
+
     def show(self):
         print(self.hour, ':', self.minute, ':',self.second)
     
@@ -96,14 +126,22 @@ b = Time(4, 17, 50)
 b.show()
 
 c = Time(0, 0, 58741)
-c.second_to_time()
 c.show()
 
+second_to_time = c.second_to_time()
+second_to_time.show()
+
+
 time_to_second = a.time_to_second()
-print(time_to_second)
+time_to_second.show()
 
 sum = a.sum(b)
 sum.show()
 
 sub = a.sub(b)
+sub.show()
+
+gmt_to_tehran_tim = a.GMT_to_tehran_time()
+gmt_to_tehran_tim.show()
+
 sub.show()
